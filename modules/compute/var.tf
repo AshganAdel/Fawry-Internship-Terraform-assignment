7d34abaf05f5bc1ccd3cc99f0aacdaf3777f1fb7
AshganAdel <adelashgan9@gmail.com>
2025-07-03 19:33:10 +0300

    Update var.tf

--- modules/compute/var.tf
+++ modules/compute/var.tf
@@ -2,3 +2,4 @@ variable "vpc_id"              { type = string }
 variable "instance_count"      { type = number }
 variable "public_subnet_ids"   { type = list(string) }
 variable "env"              { type = string }
+variable "cidr_block"   { type = string }
