9e9f54a22ab14e02ab3e2ab6e9b72748c178c03f
AshganAdel <adelashgan9@gmail.com>
2025-07-06 14:07:46 +0300

    Update ecr.tf

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -1,8 +1,26 @@
-resource "aws_ecr_repository" "my_ecr_repo" {
-    name                 = "poc-prod-my-ecr-repo" 
-    image_tag_mutability = "MUTABLE"
+resource "aws_ecrpublic_repository" "my_ecr_repo" {
+  repository_name = "poc-prod-my-ecr-repo" 
 
-    image_scanning_configuration {
-      scan_on_push = true
-    }
+  image_scanning_configuration {
+    scan_on_push = true                   
+  }
+}
+resource "aws_ecrpublic_repository_policy" "allow_public_read" {
+  repository = aws_ecrpublic_repository.my_ecr_repo.poc-prod-my-ecr-repo
+
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Sid       = "AllowPull"
+        Effect    = "Allow"
+        Principal = { AWS = "*" }             
+        Action    = [
+          "ecr-public:BatchCheckLayerAvailability",
+          "ecr-public:GetDownloadUrlForLayer",
+          "ecr-public:BatchGetImage"
+        ]
+      }
+    ]
+  })
 }
